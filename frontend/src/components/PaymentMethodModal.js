dc5b658a5eabfdda1119caf3f36a5e32293a4118
Maks-oss <mpavlov198@gmail.com>
2024-05-21 01:15:23 +0200

    added some customization

---
+++ frontend/src/components/PaymentMethodModal.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import {
+    Container,
+    Radio,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    RadioGroup,
+    Button,
+    Box,
+    Typography
+} from '@mui/material';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from '@mui/material/Backdrop';
+import LinearProgress from '@mui/material/LinearProgress';
+
+const paymentMethods = [
+    {value: 'creditCard', label: 'Credit Card'},
+    {value: 'paypal', label: 'PayPal'},
+    {value: 'bank', label: 'Bank Transfer'}
+];
+
+const ChoosePaymentMethod = () => {
+    const [selectedMethod, setSelectedMethod] = useState('');
+    const [confirm, setConfirm] = useState(false)
+    const handleChange = (event) => {
+        setSelectedMethod(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        setConfirm(true)
+    };
+    if (!confirm) {
+        return (
+            <Container maxWidth="sm">
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Choose Payment Method</FormLabel>
+                    <RadioGroup value={selectedMethod} onChange={handleChange}>
+                        {paymentMethods.map((method) => (
+                            <FormControlLabel
+                                key={method.value}
+                                value={method.value}
+                                control={<Radio/>}
+                                label={
+                                    <Box display="flex" alignItems="center">
+                                        {method.icon}
+                                        <Box ml={1}>{method.label}</Box>
+                                    </Box>
+                                }
+                            />
+                        ))}
+                    </RadioGroup>
+                </FormControl>
+                <Box mt={2}>
+                    <Button variant="contained" color="primary" onClick={handleSubmit} disabled={!selectedMethod}>
+                        Confirm Payment
+                    </Button>
+                </Box>
+            </Container>
+        );
+    } else {
+        return (
+            <div>
+                <Typography id="transition-modal-title" variant="h6" component="h2" sx={{marginBottom: '10px'}}>
+                    Processing payment...
+                </Typography>
+                <LinearProgress/>
+            </div>
+        )
+    }
+};
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+const PaymentMethodModal = ({open, handleClose}) => {
+    return (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            slots={{backdrop: Backdrop}}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                },
+            }}
+        >
+            <Fade in={open}>
+                <Box sx={modalStyle}>
+                    <ChoosePaymentMethod/>
+                </Box>
+            </Fade>
+        </Modal>)
+}
+
+export default PaymentMethodModal
--- frontend/src/components/seats/SeatSelection.js
+++ frontend/src/components/seats/SeatSelection.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react';
 import './Seats.css';
 import {Button} from '@mui/material';
 import Fade from '@mui/material/Fade';
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
+import {Box, Typography} from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
+import PaymentMethodModal from "../PaymentMethodModal";
 
 const SeatSelection = ({seats}) => {
     const [selectedSeats, setSelectedSeats] = useState([]);
     const [price, setPrice] = useState(10); // Assuming each seat costs $10
     const [isConfirm, setIsConfirm] = React.useState(false);
+    const [openModal, setOpenModal] = React.useState(false);
+
 
     const rows = groupSeatsByRow(seats);
 
@@ -25,6 +32,9 @@ const SeatSelection = ({seats}) => {
     const calculateTotalPrice = () => {
         return selectedSeats.length * price;
     };
+
+    const handleOpen = () => setOpenModal(true);
+    const handleClose = () => setOpenModal(false);
 
     return (
         <div className="container">
@@ -59,16 +69,12 @@ const SeatSelection = ({seats}) => {
                     </div>
                 ))}
             </div>
-
-            {/*<p className="text">*/}
-            {/*    You have selected <span id="count">{selectedSeats.length}</span> seats for a price of $*/}
-            {/*    <span id="total">{calculateTotalPrice()}</span>*/}
-            {/*</p>*/}
             <Fade in={isConfirm}>
-                <Button size="medium" color="secondary" variant="outlined" sx={{ flexShrink: 0, borderRadius: '20px', margin: '10px' }} onClick={() => alert(calculateTotalPrice())} >
+                <Button size="medium" color="secondary" variant="outlined" sx={{ flexShrink: 0, borderRadius: '20px', margin: '10px' }} onClick={handleOpen} >
                     Confirm
                 </Button>
             </Fade>
+            <PaymentMethodModal open={openModal} handleClose={handleClose}/>
         </div>
     );
 };
@@ -77,7 +83,6 @@ function groupSeatsByRow(seats) {
         const row = seat.row;
         if (!acc[row]) {
             acc[row] = {
-                // row: seat.row,
                 rowSeats: []
             };
         }
