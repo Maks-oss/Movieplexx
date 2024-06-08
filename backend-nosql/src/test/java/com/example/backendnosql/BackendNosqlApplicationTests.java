9903847b8633dc96237e01470c96a07f8a6bda43
Maks-oss <mpavlov198@gmail.com>
2024-06-08 23:42:15 +0200

    just some skatch
    just some skatch

---
+++ backend-nosql/src/test/java/com/example/backendnosql/BackendNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
